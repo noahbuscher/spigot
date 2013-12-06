b59969494b27028506fa500ea067f1ff23432618
Noah Buscher <noah@codingbean.com>
2013-12-05 22:03:09 -0600

    Removed unneeded code.

--- system/boot.php
+++ system/boot.php
@@ -1,14 +1,6 @@
 <?php
 
 /**
- * Libraries
- */
-$files = glob(APP . 'libraries' . DS . '/*' . EXT);
-foreach ($files as $file) {
-    require_once($file);   
-}
-
-/**
  * Functions
  */
 $files = glob(SYS . 'functions' . DS . '/*' . EXT);
