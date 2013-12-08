b53e70f1a5bfc47fc87b513da9d74b2da11e6459
Bruno Miguel <brunoalexandremiguel@gmail.com>
2013-12-08 18:25:09 +0000

    updated upstream, with theme tweaks for navigation

--- theme/index.php
+++ theme/index.php
@@ -1,9 +1,10 @@
-<html>
+<html lang="en">
 	<head>
 		<title>Spigot</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:200,100,300,400,600' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="<?php echo the_stylesheet(); ?>">
 	</head>
 	<body>
@@ -19,6 +20,7 @@
 			<?php endwhile; ?>
 		</div>
 		<footer>
+			<div class="pagination"><?php pagination(); ?></div>
 			<a href="https://github.com/Codingbean/Spigot">Spigot</a>
 		</footer>
 	</body>
