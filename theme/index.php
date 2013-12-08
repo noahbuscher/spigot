935055786f00402b7c1cb19c9454de7a2dde8f07
Bruno Miguel <brunoalexandremiguel@gmail.com>
2013-12-08 03:21:22 +0000

    Refreshed theme, with a more clean and flat look

--- theme/index.php
+++ theme/index.php
@@ -2,7 +2,8 @@
 	<head>
 		<title>Spigot</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:200,100,300,400,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="<?php echo the_stylesheet(); ?>">
 	</head>
 	<body>
