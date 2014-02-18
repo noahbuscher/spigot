06bf5fbd9832d217ded7bc56df7dc148a6eb1485
Scott O'Hara <scottaohara@gmail.com>
2014-02-17 21:10:44 -0500

    css & meta data clean up
    style.css
    remove some redundant css ( e.g. there were two .pagination>a declarations)
    
    alphabetize css cause it's easier to read that way :)
    
    change h2 selector to .date
    
    index.php
    combine google font calls into a single call
    add .date to h2 that pulls in the date
    remove type="text/css" as it's not needed in html doctype.
    Added <!doctype html>

--- theme/index.php
+++ theme/index.php
@@ -1,18 +1,18 @@
+<!doctype html>
 <html lang="en">
 	<head>
 		<title>Spigot</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" href="<?php echo the_stylesheet(); ?>">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300|Roboto+Slab:300' rel='stylesheet'>
+		<link href="<?php echo the_stylesheet(); ?>" rel="stylesheet">
 	</head>
 	<body>
 		<div class="wrap">
 			<?php while(have_feed()) : thefeed() ?>
 					<div class="item">
 						<a href="<?php echo the_link(); ?>"><h1><?php echo the_title(); ?></h1></a>
-						<h2><?php echo the_date(); ?></h2>
+						<h2 class="date"><?php echo the_date(); ?></h2>
 						<p>
 							<?php echo the_description(); ?>
 						</p>
